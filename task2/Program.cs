27a2eb4de0e4f9cbc09f2028f9fe0308213203e0
Nikita Smetannikov <smetannikovnikita@yandex.ru>
2022-09-13 14:43:42 +0300

    Task resolved

---
+++ task2/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу,
+// которая будет находить строку с наименьшей суммой элементов.
+
+int [,] array = new int[6,4];
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($" {arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+string GetSmallestAmountRow(int[,] arr)
+{
+    int[] sumsArray = new int[arr.GetLength(0)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            sumsArray[i] += arr[i, j];
+        }
+    }
+
+    int index_min = 0;
+    int min = sumsArray[0];
+    for (int i = 1; i < sumsArray.Length; i++)
+    {
+        if (min > sumsArray[i])
+        {
+            min = sumsArray[i];
+            index_min = i;
+        }
+    }
+    
+    return $"Номер строки с наименьшей суммой элементов: {index_min + 1}";
+}
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.Write(GetSmallestAmountRow(array));
